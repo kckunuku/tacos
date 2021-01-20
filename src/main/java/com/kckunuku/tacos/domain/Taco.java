9bea6f1fda5ec1cb916d79a99696c2f3fdbaf3bc
kckunuku <51337209+kckunuku@users.noreply.github.com>
2021-01-19 20:20:08 -0600

    order form and order submission handler added

---
+++ src/main/java/com/kckunuku/tacos/domain/Taco.java
@@ -0,0 +1,11 @@
+package com.kckunuku.tacos.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+}
